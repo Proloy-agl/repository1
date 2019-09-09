93f275836bc797a048ad9c7b1856c6399acd8d0e
a30003100 <a30003100@agl.com.au>
2019-09-09 13:19:52 +1000

    commit

--- CMDB2/WebForm3.aspx.cs
+++ CMDB2/WebForm3.aspx.cs
@@ -255,7 +255,97 @@ namespace CMDB2
                           
 
             }
-          
+
+
+            //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            ///////////////////////////////////Generate Token/////////////////////////////////////////////////////////////
+            var client = new RestClient("https://jirauat.mobile.agl.com.au/api/jwt/login");
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("cache-control", "no-cache");
+            request.AddHeader("Connection", "keep-alive");
+            request.AddHeader("Content-Length", "41");
+            request.AddHeader("Accept-Encoding", "gzip, deflate");
+            request.AddHeader("Host", "jirauat.mobile.agl.com.au");
+            request.AddHeader("Postman-Token", "2332ba70-dcf1-4e0b-b248-8ad5c17500b3,f1624587-65bb-4734-87f7-25dc90c7e437");
+            request.AddHeader("Cache-Control", "no-cache");
+            request.AddHeader("Accept", "*/*");
+            request.AddHeader("User-Agent", "PostmanRuntime/7.16.3");
+            request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
+            request.AddParameter("undefined", "username=svc_magneto&password=svc_magneto", ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+            var data = response.Content;
+
+            string data1 = "AR-JWT";
+            string tokenPass = string.Concat(data1, " ", data);
+            //////////////////////////////////Get CI items ///////////////////////////////////////////////////////////////////
+            var client10 = new RestClient("https://jirauat.mobile.agl.com.au/api/arsys/v1/entry/CHG:Associations?q=%27Request%20ID02%27=%22CRQ000000087001%20%22");
+            var request10 = new RestRequest(Method.GET);
+            request10.AddHeader("cache-control", "no-cache");
+            request10.AddHeader("Connection", "keep-alive");
+            request10.AddHeader("Accept-Encoding", "gzip, deflate");
+            request.AddHeader("Host", "jirauat.mobile.agl.com.au");
+            request10.AddHeader("Postman-Token", "b9f14d6d-19ca-4d17-a820-daf1154bd2db,5fd63ab8-cfe2-456d-96f3-45b5da106246");
+            request10.AddHeader("Cache-Control", "no-cache");
+            request10.AddHeader("Accept", "*/*");
+            request10.AddHeader("User-Agent", "PostmanRuntime/7.16.3");
+            request10.AddHeader("Content-Type", "application/json");
+            request10.AddHeader("Authorization", tokenPass);
+            IRestResponse response10 = client10.Execute(request10);
+
+            JObject jsonResponse10 = (JObject)JsonConvert.DeserializeObject(response10.Content);
+
+            dynamic json = JValue.Parse(response10.Content);
+
+            ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+            //      Label2.Text = Convert.ToString( jsonResponse10) ;
+
+            List<string> AppCI = new List<string>();
+            List<string> InfraCI = new List<string>();
+            AppCI.Add("Select");
+            InfraCI.Add("Select");
+
+            var js10 = new JavaScriptSerializer();
+            var d10 = js10.Deserialize<dynamic>(Convert.ToString(jsonResponse10));
+            int m = 1;
+            int n = 0;
+            dynamic jsonObj = JsonConvert.DeserializeObject(response10.Content);
+
+
+            Dictionary<string, object> csObj =
+    js10.Deserialize<Dictionary<string, object>>(response10.Content);
+            n = ((ArrayList)csObj["entries"]).Count;
+            // Label2.Text = Convert.ToString(n);
+
+
+
+
+            for (int x = 0; x < n; x++)
+            {
+
+                if (d10["entries"][x]["values"]["Lookup Keyword"] == "BMC_APPLICATION")
+                {
+                    string Value1 = d10["entries"][x]["values"]["Request Description01"];
+                    AppCI.Add(Value1);
+                }
+
+                if (d10["entries"][x]["values"]["Lookup Keyword"] == "BMC_COMPUTERSYSTEM")
+                {
+                    string Value2 = d10["entries"][x]["values"]["Request Description01"];
+                    InfraCI.Add(Value2);
+                }
+
+            }
+
+
+
+            DL3.DataSource = AppCI;
+            DL3.DataBind();
+            DL4.DataSource = InfraCI;
+            DL4.DataBind();
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
 
         }
 

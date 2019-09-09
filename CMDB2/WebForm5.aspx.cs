7e84d6c409b7a09652fc4aeb32b51aec3a71cd98
a30003100 <a30003100@agl.com.au>
2019-09-09 15:20:20 +1000

    commit

--- CMDB2/WebForm5.aspx.cs
+++ CMDB2/WebForm5.aspx.cs
@@ -52,8 +52,8 @@ namespace CMDB2
             if (CheckBox7.Checked) { choiceB = Convert.ToString("1"); Session["value2"] = choiceB; } else { choiceB = Convert.ToString("5"); Session["value2"] = "5"; };
             if (CheckBox8.Checked) { choiceC = Convert.ToString("1"); Session["value3"] = choiceC; } else { choiceC = Convert.ToString("5"); Session["value3"] = "5"; };
             if (CheckBox9.Checked) { choiceD = Convert.ToString("1"); Session["value4"] = choiceD; } else { choiceD = Convert.ToString("5"); Session["value4"] = "5"; };
-            if (CheckBox10.Checked) { choiceE = Convert.ToString("1"); Session["value5"] = choiceE; } else { choiceE = Convert.ToString("5"); Session["value5"] = "5"; };
-            //if (CheckBox6.Checked) { choiceF = Convert.ToString("1"); Session["value6"] = choiceF; } else { choiceF = Convert.ToString("5"); Session["value6"] = "5"; };
+            if (CheckBox10.Checked){ choiceE = Convert.ToString("1"); Session["value5"] = choiceE; } else { choiceE = Convert.ToString("5"); Session["value5"] = "5"; };
+          //if (CheckBox6.Checked) { choiceF = Convert.ToString("1"); Session["value6"] = choiceF; } else { choiceF = Convert.ToString("5"); Session["value6"] = "5"; };
 
             if ((choiceA == "5") && (choiceB == "5") && (choiceC == "5") && (choiceD == "5") && (choiceE == "5") && (choiceF == "5"))
             {

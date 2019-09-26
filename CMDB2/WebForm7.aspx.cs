042077719dddbaad092bc2617abf51213293268b
a30003100 <a30003100@agl.com.au>
2019-09-26 21:49:32 +1000

    commit

--- CMDB2/WebForm7.aspx.cs
+++ CMDB2/WebForm7.aspx.cs
@@ -77,9 +77,9 @@ namespace CMDB2
 
             
 
-            var jsonObj = JsonConvert.DeserializeObject(response55.Content);
+            dynamic jsonObj = JsonConvert.DeserializeObject(response55.Content);
 
-            var json1 = JObject.Parse(Convert.ToString(jsonObj));
+            dynamic json1 = JObject.Parse(Convert.ToString(jsonObj));
 
            // dynamic json = JValue.Parse(response55.Content);
 

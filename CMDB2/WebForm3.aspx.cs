ff7ac058e6f17b1799393690dd47fd1e2afc6c0e
a30003100 <a30003100@agl.com.au>
2019-10-03 21:55:01 +1000

    commit

--- CMDB2/WebForm3.aspx.cs
+++ CMDB2/WebForm3.aspx.cs
@@ -627,7 +627,7 @@ namespace CMDB2
 
 
 
-                    cmd.CommandText = "insert into [Audit_Relationship_App_to_Infra](CR_Number,App_CI_Name,Infra_CI_Name,Transaction_Timestamp,Transaction_Status,Environment) values (@var1,@var2,@var3,@var4,@var5,@var6)";
+                    cmd.CommandText = "insert into [Audit_Relationship_App_to_Infra_Magneto](CR_Number,App_CI_Name,Infra_CI_Name,Transaction_Timestamp,Transaction_Status,Environment) values (@var1,@var2,@var3,@var4,@var5,@var6)";
                     cmd.Parameters.AddWithValue("@var1", CR_1);
                     cmd.Parameters.AddWithValue("@var2", c1);
                     cmd.Parameters.AddWithValue("@var3", c2);
@@ -701,7 +701,7 @@ namespace CMDB2
                 {
                     cmd.Connection = conn;
                     cmd.CommandType = CommandType.Text;
-                    cmd.CommandText = "insert into [Audit_Relationship_Infra_to_Infra](CR_Number,Parent_Infra_CI_Name,Child_Infra_CI_Name,Transaction_Timestamp,Transaction_Status) values (@var1,@var2,@var3,@var4,@var5)";
+                    cmd.CommandText = "insert into [Audit_Relationship_Infra_to_Infra_Magneto](CR_Number,Parent_Infra_CI_Name,Child_Infra_CI_Name,Transaction_Timestamp,Transaction_Status) values (@var1,@var2,@var3,@var4,@var5)";
                     cmd.Parameters.AddWithValue("@var1", CR_2);
                     cmd.Parameters.AddWithValue("@var2", c1);
                     cmd.Parameters.AddWithValue("@var3", c2);
